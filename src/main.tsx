443eeb70fb40ae0eccb4f8aa77807acb91af3d9c
Olek Golus <olekgolus02@gmail.com>
2023-01-28 00:25:16 +0100

    Fix vulnerabilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.scss";
 import { HOME_PATH } from "./constants/RouterConstants";
 
@@ -12,11 +9,11 @@ const router = createBrowserRouter([
   {
     path: HOME_PATH,
     element: <App />,
-  }
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
