883fa527559cb808258675e2e7bf27159fa3f6bf
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-02-02 17:42:52 +0100

    Add button styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,9 +5,9 @@ const HomePage = () => {
     <>
       HomePage
       <div className={`${Button.background}`}>
-        <button className={`${Button.button} ${Button.square} ${Button.filled}`}></button>
-        <button className={`${Button.button} ${Button.round} ${Button.filled}`}></button>
-        <button className={`${Button.button} ${Button.round} ${Button.outlined}`}></button>
+        <button className={`${Button.button} ${Button.square} ${Button.filled}`}>Zapisz się</button>
+        <button className={`${Button.button} ${Button.round} ${Button.filled}`}>Zapisz się</button>
+        <button className={`${Button.button} ${Button.round} ${Button.outlined}`}>Dowiedz się więcej</button>
       </div>
     </>
   );
