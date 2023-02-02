b06d74f0169b33332d68f00f33fd48981650dff6
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-02-02 16:39:51 +0100

    add-button

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,17 @@
-import '../styles/Constants.scss';
+import Button from '../styles/Button.module.scss';
 
 const HomePage = () => {
-  return <div className="site">Siema</div>;
+  return (
+    <>
+      HomePage
+      <div className={`${Button.background}`}>
+        <button className={`${Button.button} ${Button.square} ${Button.filled}`}></button>
+      <button className={`${Button.button} ${Button.round} ${Button.filled}`}></button>
+      <button className={`${Button.button} ${Button.round} ${Button.outlined}`}>Jebać SWIFTA</button>
+      </div>
+      
+    </>
+  );
 };
 
 export default HomePage;
