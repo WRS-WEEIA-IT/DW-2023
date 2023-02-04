dd9d23e6096cd030d202dd87e47800d89bc88ea4
Olek Golus <olekgolus02@gmail.com>
2023-02-04 22:05:13 +0100

    Setup basic structure

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,24 +1,25 @@
-import Button from '../styles/Button.module.scss';
-import Card from './Card/Card';
-import mockImage from '../assets/workshopImage2.png';
+import Navbar from './Navbar/Navbar';
 
 const HomePage = () => {
   return (
-    <>
-      <div className={`${Button.background}`}>
-        <button className={`${Button.button} ${Button.square} ${Button.filled}`}>Zapisz się</button>
-        <button className={`${Button.button} ${Button.round} ${Button.filled}`}>Zapisz się</button>
-        <button className={`${Button.button} ${Button.round} ${Button.outlined}`}>
-          Dowiedz się więcej
-        </button>
-      </div>
-      <div id="card-grid">
-        <Card imageSrc={mockImage} eventType="workshop" title="Workshop " />
-        <Card imageSrc={mockImage} eventType="workshop" title="Workshop " />
-        <Card imageSrc={mockImage} eventType="workshop" title="Workshop " />
-        <Card imageSrc={mockImage} eventType="workshop" title="Workshop" />
-      </div>
-    </>
+    <div className="background">
+      <Navbar />
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+      <h1>Scroll test</h1>
+    </div>
   );
 };
 
