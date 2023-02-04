517add395a888aac5dac29cc5c3a25b47d82e7e3
Olek Golus <olekgolus02@gmail.com>
2023-02-04 19:50:39 +0100

    Fix button styling
    Change button borders

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,11 +3,12 @@ import Button from '../styles/Button.module.scss';
 const HomePage = () => {
   return (
     <>
-      HomePage
       <div className={`${Button.background}`}>
         <button className={`${Button.button} ${Button.square} ${Button.filled}`}>Zapisz się</button>
         <button className={`${Button.button} ${Button.round} ${Button.filled}`}>Zapisz się</button>
-        <button className={`${Button.button} ${Button.round} ${Button.outlined}`}>Dowiedz się więcej</button>
+        <button className={`${Button.button} ${Button.round} ${Button.outlined}`}>
+          Dowiedz się więcej
+        </button>
       </div>
     </>
   );
