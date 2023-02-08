ae2f096d4bd6522e473553deeeb7de88ff8de39a
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-02-08 19:43:05 +0100

    add grid wrapper

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
+import Card from '../Card/Card';
+import '../../styles/CardGrid.scss'
 
-const CardGrid = () => {
-    return;
-}
+
+
+const CardGrid = ({ CardsName }: { CardsName: string[] }) => {
+  return (
+    <div className='cards-container'>
+      {CardsName.map((name: string, index: number) => (
+        <Card key={index} imageSrc='../../assets/worshopImage2.png' eventType="training" title={name} />
+      ))}
+    </div>
+  );
+};
+
+export default CardGrid;
