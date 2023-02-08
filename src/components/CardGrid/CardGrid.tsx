c706117b2fb1972c5af7813375e9344b4f359e4e
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-02-08 22:28:11 +0100

    add mobile version

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import Card from '../Card/Card';
-import '../../styles/CardGrid.scss'
-
-
+import '../../styles/CardGrid.scss';
 
 const CardGrid = ({ CardsName }: { CardsName: string[] }) => {
+  const moreThanFour = true;
+  const CARDS_IN_ROW = 4;
+
   return (
-    <div className='cards-container'>
-      {CardsName.map((name: string, index: number) => (
-        <Card key={index} imageSrc='../../assets/worshopImage2.png' eventType="training" title={name} />
-      ))}
+    <div className="grid-container">
+      {CardsName.map(
+        (name: string, index: number) =>
+          (index < CARDS_IN_ROW && (
+            <Card
+              key={index}
+              imageSrc="../../assets/worshopImage2.png"
+              eventType="training"
+              title={name}
+            />
+          )) ||
+          (index >= CARDS_IN_ROW && moreThanFour && (
+            <Card
+              key={index}
+              imageSrc="../../assets/worshopImage2.png"
+              eventType="training"
+              title={name}
+            />
+          ))
+      )}
     </div>
   );
 };
