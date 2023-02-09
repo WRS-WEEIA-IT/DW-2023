e9c4612d3e9435b84a0d555b448aa67136745aa8
Olek Golus <olekgolus02@gmail.com>
2023-02-09 16:34:07 +0100

    Clean code

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -7,36 +7,25 @@ import CardInterface from './../Card/CardInterface';
 
 const CardGrid = ({ showAllCards = false }: { showAllCards?: boolean }) => {
   const DEFAULT_VISIBLE_CARDS_COUNT = 4;
+
   const [events] = useCollectionData<CardInterface>(
     collection(firebaseDb, firebaseCollectionName) as Query<CardInterface>
   );
+  const filteredEvents = showAllCards ? events : events?.slice(0, DEFAULT_VISIBLE_CARDS_COUNT);
 
   return (
     <div className="grid-container">
-      {events &&
-        events.map(
-          (event, index: number) =>
-            (index < DEFAULT_VISIBLE_CARDS_COUNT && (
-              <Card
-                key={index}
-                imageSource={event.imageSource}
-                eventType={event.eventType}
-                title={event.title}
-                timeStart={event.timeStart}
-                timeEnd={event.timeEnd}
-              />
-            )) ||
-            (index >= DEFAULT_VISIBLE_CARDS_COUNT && showAllCards && (
-              <Card
-                key={index}
-                imageSource={event.imageSource}
-                eventType={event.eventType}
-                title={event.title}
-                timeStart={event.timeStart}
-                timeEnd={event.timeEnd}
-              />
-            ))
-        )}
+      {filteredEvents &&
+        filteredEvents.map((event, index: number) => (
+          <Card
+            key={index}
+            imageSource={event.imageSource}
+            eventType={event.eventType}
+            title={event.title}
+            timeStart={event.timeStart}
+            timeEnd={event.timeEnd}
+          />
+        ))}
     </div>
   );
 };
