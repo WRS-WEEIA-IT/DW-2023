c036d624f002e47c8024d68779611c2f2f6234d1
Olek Golus <olekgolus02@gmail.com>
2023-02-09 16:26:14 +0100

    Add Cards firebase support, add showAllCards boolean property, few small fixes

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,31 +1,15 @@
 import Navbar from '../Navbar/Navbar';
 import './HomePage.scss';
-import { firebaseCollectionName, firebaseDb } from '../../FirebaseConfig';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { collection, Query } from 'firebase/firestore';
-import CardInterface from './../Card/CardInterface';
-import Card from '../Card/Card';
 import Clock from '../Clock/Clock';
+import CardGrid from '../CardGrid/CardGrid';
 
 const HomePage = () => {
-  const [events] = useCollectionData<CardInterface>(
-    collection(firebaseDb, firebaseCollectionName) as Query<CardInterface>
-  );
-  if (!events) return <> </>;
-  const event = events[0];
-
   return (
     <div className="background">
       <Navbar />
       <div id="homepage-content">
-        <Card
-          title={event.title}
-          eventType={event.eventType}
-          imageSource={event.imageSource}
-          timeStart={event.timeStart}
-          timeEnd={event.timeEnd}
-        />
         <Clock />
+        <CardGrid showAllCards />
         <h1>Scroll test</h1>
         <h1>Scroll test</h1>
         <h1>Scroll test</h1>
