016c28aef5a918361b70b2e51afedf5459e5ab4f
Olek Golus <olekgolus02@gmail.com>
2023-02-09 15:06:28 +0100

    Clean code

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import { useState, useContext } from 'react';
-import '../../styles/Clock.scss';
+import './Clock.scss';
 import { LanguageModeContext } from '../../contexts/LanguageContext';
 
 const Clock = () => {
   const ONE_MINUTE = 60000;
-  const DW_FINAL_DATE = '2023-03-31';
   const ONE_HOUR = 3600000;
   const ONE_DAY = 86400000;
+  const DW_FINAL_DATE = '2023-03-31';
   const finalDate = new Date(DW_FINAL_DATE);
 
   const countDaysLeft = () => {
@@ -15,16 +14,10 @@ const Clock = () => {
     const counterResult: number = Math.floor(
       (+finalDate.getTime() - +currentDate.getTime() + ONE_HOUR) / ONE_DAY + 1
     );
-    if (counterResult > 0) {
-      return counterResult;
-    } else if (counterResult === 0) {
-      return 0;
-    } else {
-      return counterResult;
-    }
+    return counterResult != 0 ? counterResult : 0;
   };
 
-  const [DaysLeft, setDaysLeft] = useState(countDaysLeft());
+  const [daysLeft, setDaysLeft] = useState(countDaysLeft());
   const language = useContext(LanguageModeContext);
 
   setInterval(() => {
@@ -32,18 +25,17 @@ const Clock = () => {
   }, ONE_MINUTE);
 
   const setCounterInfo = () => {
-    const isCounterValid = countDaysLeft();
-    if (isCounterValid > 0) {
+    if (daysLeft > 0) {
       return (
         <>
           <p className="date-label">{language == 'polish' ? '31 MARCA 2023' : '31 MARCH 2023'}</p>
-          <h1 className="date-counter">{DaysLeft}</h1>
+          <h1 className="date-counter">{daysLeft}</h1>
           <p className="counter-label">
             {language == 'polish' ? 'DNI DO WYDARZENIA' : 'DAYS TO EVENT'}
           </p>
         </>
       );
-    } else if (isCounterValid === 0) {
+    } else if (daysLeft === 0) {
       return (
         <>
           <h3 className="date-counter-actual">
@@ -58,7 +50,7 @@ const Clock = () => {
           <p className="date-label">
             {language == 'polish' ? 'Wydarzenie odbyło się' : 'The event was'}
           </p>
-          <h1 className="date-counter">{-DaysLeft}</h1>
+          <h1 className="date-counter">{-daysLeft}</h1>
           <p className="counter-label">{language == 'polish' ? 'DNI TEMU' : 'DAYS AGO'}</p>
         </>
       );
