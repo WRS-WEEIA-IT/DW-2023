b475a5cf9b5e7f5b4ccfbcaabe7eee9b29fe0c87
olasgolas11 <olekgolus02@gmail.com>
2023-02-14 19:39:34 +0100

    Clean CSS, clean sections variables, clean sections

--- src/components/HomePageBanner/HomePageBanner.tsx
+++ src/components/HomePageBanner/HomePageBanner.tsx
@@ -2,19 +2,26 @@ import Clock from '../Clock/Clock';
 import './HomePageBanner.scss';
 import Button from '../../styles/Button.module.scss';
 import DW_LOGO_IMG from '../../../public/images/dw_logo.png';
+import { useContext } from 'react';
+import { LanguageModeContext } from '../../contexts/LanguageContext';
 
 const HomePageBanner = () => {
+  const languageMode = useContext(LanguageModeContext);
+
+  const BANNER_TEXT =
+    languageMode == 'polish' ? 'Twoja nowa perspektywa na EEIA' : 'Your new perspective on EEIA';
+
   return (
     <div id="home-page-banner">
       <div id="left-banner-side">
         <img id="dw-logo" src={DW_LOGO_IMG} />
-        <h4 id="banner-text">Twoja nowa perspektywa na EEIA</h4>
+        <h4 id="banner-text">{BANNER_TEXT}</h4>
         <div id="banner-buttons">
           <button className={`${Button.button} ${Button.filled} ${Button.round}`}>
-            Zapisz się
+            {languageMode === 'polish' ? 'Zapisz się' : 'Sign up'}
           </button>
           <button className={`${Button.button} ${Button.outlined} ${Button.round}`}>
-            Dowiedz się więcej
+            {languageMode === 'polish' ? 'Dowiedz się więcej' : 'Find out more'}
           </button>
         </div>
       </div>
--- src/components/Sections/PartnersSection/PartnersSection.tsx
+++ src/components/Sections/PartnersSection/PartnersSection.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { LanguageModeContext } from '../../../contexts/LanguageContext';
 import './PartnersSection.scss';
 
 const PartnersSection = () => {
+  const languageMode = useContext(LanguageModeContext);
+
   return (
     <>
       <div id="partners-header-container">
-        <h2 id="partners-header">Partnerzy</h2>
+        <h2 id="partners-header">{languageMode == 'polish' ? 'Partnerzy' : 'Partners'}</h2>
       </div>
     </>
   );
