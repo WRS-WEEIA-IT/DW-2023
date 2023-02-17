f25916d2a45665134ba6fb45b3233c13b9da0efb
olasgolas11 <olekgolus02@gmail.com>
2023-02-17 20:22:50 +0100

    Fix about us margin

--- src/components/Sections/DescriptionSection/DescriptionTexts.ts
+++ src/components/Sections/DescriptionSection/DescriptionTexts.ts
@@ -2,7 +2,7 @@ export const headerTextPL = 'Czym jest Dzień Wydziału EEIA?';
 export const headerTextEN = "What is EEIA's department day?";
 
 export const descriptionTextPL =
-  'Dzień Wydziału EEIA 2023 to już siódma edycja największego wydarzenia odbywającego się corocznie na Politechnice Łódzkiej. Biorąc pod uwagę liczbę uczestników i ilość pracodawców spotykających się z młodymi ludźmi, jest to wyjątkowa okazja dla firm na rozpowszechnienie swojego wizerunku w środowisku uczelnianym. Docierając zarówno do studentów, czyli potencjalnych pracowników oraz Władz Uczelni tworzymy możliwości stałej współpracy oraz przestrzeń na wymianę doświadczeń.';
+  'Dzień Wydziału EEIA 2023 to już siódma edycja największego wydarzenia odbywającego się corocznie na Politechnice Łódzkiej. Biorąc pod uwagę liczbę uczestników i ilość pracodawców spotykających się z młodymi ludźmi, jest to wyjątkowa okazja dla firm na rozpowszechnienie swojego wizerunku w środowisku uczelnianym. Docierając zarówno do studentów, czyli potencjalnych pracowników oraz Władz Uczelni, tworzymy możliwości stałej współpracy oraz przestrzeń na wymianę doświadczeń.';
 
 export const descriptionTextEN =
   "EEIA's department day is already a seventh edition of the biggest event that is held annually on Lodz University of Technology. ";
