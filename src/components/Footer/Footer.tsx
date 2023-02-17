e46950c6465443131dd64399dced578dc3d6c5dd
olasgolas11 <olekgolus02@gmail.com>
2023-02-17 22:45:09 +0100

    Add navbar navigation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -59,7 +59,7 @@ const Footer = () => {
   };
 
   return (
-    <div className="footer-container">
+    <div className="footer-container" id="footer-section">
       {contactSection()}
       {footerSection()}
       <div className="rights-section">
