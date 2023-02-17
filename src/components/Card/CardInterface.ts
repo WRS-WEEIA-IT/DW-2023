b0c06c3485205af52a9d799fe56a8b3ec2be8591
olasgolas11 <olekgolus02@gmail.com>
2023-02-18 00:17:27 +0100

    Fix footer texts

--- src/components/Card/CardInterface.ts
+++ src/components/Card/CardInterface.ts
@@ -1,7 +1,9 @@
+import { Timestamp } from '@firebase/firestore';
+
 export default interface CardInterface {
   eventType: 'workshop' | 'lecture';
   imageSource: string;
-  timeEnd: Date;
-  timeStart: Date;
+  timeEnd: Timestamp;
+  timeStart: Timestamp;
   title: string;
 }
