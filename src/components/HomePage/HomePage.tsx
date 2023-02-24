310f27d14c7964ad02625670acefae9bb0a00af8
Olek Golus <olekgolus02@gmail.com>
2023-02-24 12:31:51 +0100

    Add modal to page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import EventsSection from '../Sections/EventsSection/EventsSection';
 import AboutSection from '../Sections/DescriptionSection/AboutSection';
 import PartnersSection from '../Sections/PartnersSection/PartnersSection';
 import Footer from '../Footer/Footer';
+<<<<<<< HEAD
 import { domAnimation, LazyMotion } from 'framer-motion';
 
 const HomePage = () => {
@@ -19,6 +20,19 @@ const HomePage = () => {
         <Footer />
       </div>
     </LazyMotion>
+=======
+
+const HomePage = () => {
+  return (
+    <div id="homepage-container">
+      <Navbar />
+      <WelcomeSection />
+      <AboutSection />
+      <EventsSection />
+      <PartnersSection />
+      <Footer />
+    </div>
+>>>>>>> 5291e98 (Add modal to page)
   );
 };
 
