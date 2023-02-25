243fbd6178d467dfe2549cbb81a2e618d1c2f404
Olek Golus <olekgolus02@gmail.com>
2023-02-25 16:04:53 +0100

    Fix email service

--- src/services/EmailService.tsx
+++ src/services/EmailService.tsx
@@ -1,42 +1,4 @@
-export const Email = {
-  send: function (a) {
-    return new Promise(function (n, e) {
-      (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = 'Send');
-      const t = JSON.stringify(a);
-      Email.ajaxPost('https://smtpjs.com/v3/smtpjs.aspx?', t, function (e) {
-        n(e);
-      });
-    });
-  },
-  ajaxPost: function (e, n, t) {
-    const a = Email.createCORSRequest('POST', e);
-    a.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'),
-      (a.onload = function () {
-        const e = a.responseText;
-        null != t && t(e);
-      }),
-      a.send(n);
-  },
-  ajax: function (e, n) {
-    const t = Email.createCORSRequest('GET', e);
-    (t.onload = function () {
-      const e = t.responseText;
-      null != n && n(e);
-    }),
-      t.send();
-  },
-  createCORSRequest: function (e, n) {
-    let t = new XMLHttpRequest();
-    return (
-      'withCredentials' in t
-        ? t.open(e, n, !0)
-        : 'undefined' != typeof XMLHttpRequest
-        ? (t = new XMLHttpRequest()).open(e, n)
-        : (t = null),
-      t
-    );
-  },
-};
+import Email from './SMTP';
 
 const createFormEmailBody = (name: string, email: string, message: string) => {
   return `<b>Name:</b> ${name}<br><b>Email:</b> ${email}<br><b>Message:</b> ${message}<br>`;
