28c135fb9fa620290c58754c1ffb9c6032dcbd7c
Olek Golus <olekgolus02@gmail.com>
2023-03-06 20:51:54 +0100

    Change about us text

---
+++ src/components/Sections/DescriptionSection/DescriptionTexts.tsx
@@ -0,0 +1,48 @@
+export const headerTextPL = 'Czym jest Dzień Wydziału EEIA?';
+export const headerTextEN = "What is EEIA's department day?";
+
+export const descriptionTextPL = () => (
+  <>
+    {
+      'Dzień Wydziału Elektrotechniki, Elektroniki, Informatyki i Automatyki Politechniki Łódzkiej to wydarzenie skierowane do studentów naszego wydziału oraz uczniów szkół średnich.'
+    }
+    <br />
+    <br />
+    {
+      'W tym roku odbędzie się ono już 31 marca, po raz pierwszy od trzech lat w formie stacjonarnej.'
+    }
+    <br />
+    <br />
+    {
+      'Studenci będą mieli możliwość zapoznać się z ofertami staży i praktyk, porozmawiać z potencjalnymi pracodawcami oraz nabyć cenne umiejętności podczas organizowanych szkoleń. Natomiast uczniowie szkół średnich będą mieli okazję poznać nasz wydział i odpowiedzi na wszelkie pytania dotyczące studiowania, a także upewnić się w wyborze odpowiedniego kierunku.'
+    }
+    <br />
+    <br />
+    {
+      'Zachęcamy do dołączenia do wydarzenia na Facebooku, gdzie można znaleźć wszystkie najważniejsze informacje dotyczące DW EEIA 2023.'
+    }
+  </>
+);
+
+export const descriptionTextEN = () => (
+  <>
+    {
+      'The Day of the Faculty of Electrical, Electronic, Computer and Control Engineering at Lodz University of Technology is an event aimed at students of our faculty and high school students.'
+    }
+    <br />
+    <br />
+    {
+      'This year it will take place on March 31st, for the first time in three years in a stationary form.'
+    }
+    <br />
+    <br />
+    {
+      'Students will have the opportunity to learn about internship and job offers, talk to potential employers, and gain valuable skills during the organized trainings. High school students, on the other hand, will have the opportunity to get to know our faculty, get answers to all questions related to studying, and ensure the choice of the appropriate field of study.'
+    }
+    <br />
+    <br />
+    {
+      'We encourage you to join the event on Facebook, where you can find all the important information about DW EEIA 2023.'
+    }
+  </>
+);
