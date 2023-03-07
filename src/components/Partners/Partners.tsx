576341ee5cb00c1155d0274f8f2cef4605c49f6d
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-03-07 21:21:39 +0100

    add partners logos

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -3,7 +3,6 @@ import { firebaseDb } from '../../FirebaseConfig';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { collection, Query } from 'firebase/firestore';
 import PartnersInterface from './PartnersInterface';
-import MOON from '../../../public/moon.png';
 
 const Partners = () => {
   const [partners, loading] = useCollectionData<PartnersInterface>(
@@ -26,29 +25,35 @@ const Partners = () => {
 
   filterPartners();
 
+  const consoleLog = (name: string) => {
+    console.log(name.replace('/', ' '));
+  };
+
   return (
     <div className="partners-container">
       <div className="diamond-section">
         {diamondPackage.map((partner, index) => (
           <div key={index} className="diamond-container">
-            <img src={MOON} className="diamond-logo"></img>
-            <h3 className="diamond-name">{partner.name}</h3>
+            <img src={`../../../public/logos/${partner.name}.png`} className="diamond-logo"></img>
+            {/* <h3 className="diamond-name">{partner.name}</h3> */}
           </div>
         ))}
       </div>
       <div className="gold-section">
         {goldPackage.map((partner, index) => (
           <div key={index} className="gold-container">
-            <img src={MOON} className="gold-logo"></img>
-            <h4 className="gold-name">{partner.name}</h4>
+            <img
+              src={`../../../public/logos/${partner.name.replaceAll('/', ' ')}.png`}
+              className="gold-logo"></img>
+            {/* <h4 className="gold-name">{partner.name}</h4> */}
           </div>
         ))}
       </div>
       <div className="silver-section">
         {silverPackage.map((partner, index) => (
           <div key={index} className="silver-container">
-            <img src={MOON} className="silver-logo"></img>
-            <h5 className="silver-name">{partner.name}</h5>
+            <img src={`../../../public/logos/${partner.name}.png`} className="silver-logo"></img>
+            {/* <h5 className="silver-name">{partner.name}</h5> */}
           </div>
         ))}
       </div>
