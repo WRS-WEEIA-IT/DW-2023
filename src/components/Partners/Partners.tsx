9f8fb9244517cca1c3ec6758301cdcdc4432895a
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-03-13 21:24:34 +0100

    add wave image

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -34,79 +34,92 @@ const Partners = () => {
   filterPartners();
 
   return (
-    <div className="partners-container">
-      <h2 id="strategic-partners-header">
-        {languageMode == 'polish' ? 'Partnerzy strategiczni' : 'Strategic partners'}
-      </h2>
-      <m.div
-        className="strategic-section"
-        initial="hidden"
-        whileInView="visible"
-        viewport={cardViewportProperties}
-        variants={createAnimateOnScroll(0.1)}>
-        {strategicPackage.map((partner, index) => (
-          <div key={index} className="strategic-container">
-            <img
-              src={`../../../public/logos/${partner.imageSource}.png`}
-              className="strategic-logo"></img>
-          </div>
-        ))}
-      </m.div>
-      <h2 id="partners-header">{languageMode == 'polish' ? 'Partnerzy' : 'Partners'}</h2>
-      <m.div
-        className="diamond-section"
-        initial="hidden"
-        whileInView="visible"
-        viewport={cardViewportProperties}
-        variants={createAnimateOnScroll(0.1)}>
-        {diamondPackage.map((partner, index) => (
-          <div key={index} className="diamond-container">
-            <img
-              src={`../../../public/logos/${partner.imageSource}.png`}
-              className="diamond-logo"></img>
-          </div>
-        ))}
-      </m.div>
-      <m.div
-        className="gold-section"
-        initial="hidden"
-        whileInView="visible"
-        viewport={cardViewportProperties}
-        variants={createAnimateOnScroll(0.1)}>
-        {goldPackage.map((partner, index) => (
-          <div key={index} className="gold-container">
-            <img
-              src={`../../../public/logos/${partner.imageSource}.png`}
-              className="gold-logo"></img>
-          </div>
-        ))}
-      </m.div>
-      <m.div
-        className="silver-section"
-        initial="hidden"
-        whileInView="visible"
-        viewport={cardViewportProperties}
-        variants={createAnimateOnScroll(0.1)}>
-        {silverPackage.map((partner, index) => (
-          <div key={index} className="silver-container">
-            <img
-              src={`../../../public/logos/${partner.imageSource}.png`}
-              className="silver-logo"></img>
-          </div>
-        ))}
-      </m.div>
-      <h2 id="patrons-header">{languageMode == 'polish' ? 'Patronat' : 'Patronage'}</h2>
-      <m.div
-        className="patrons-section"
-        initial="hidden"
-        whileInView="visible"
-        viewport={cardViewportProperties}
-        variants={createAnimateOnScroll(0.1)}>
-        <div className="patrons-container">
-          <img src="../../../public/logos/mlodzi_w_lodzi.png" className="patrons-logo"></img>
+    <>
+      <div className="partners-container">
+        <div className="wave-image">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none">
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"></path>
+          </svg>
         </div>
-      </m.div>
-    </div>
+        <h2 id="strategic-partners-header">
+          {languageMode == 'polish' ? 'Partnerzy strategiczni' : 'Strategic partners'}
+        </h2>
+        <m.div
+          className="strategic-section"
+          initial="hidden"
+          whileInView="visible"
+          viewport={cardViewportProperties}
+          variants={createAnimateOnScroll(0.1)}>
+          {strategicPackage.map((partner, index) => (
+            <div key={index} className="strategic-container">
+              <img
+                src={`../../../public/logos/${partner.imageSource}.png`}
+                className="strategic-logo"></img>
+            </div>
+          ))}
+        </m.div>
+        <h2 id="partners-header">{languageMode == 'polish' ? 'Partnerzy' : 'Partners'}</h2>
+        <m.div
+          className="diamond-section"
+          initial="hidden"
+          whileInView="visible"
+          viewport={cardViewportProperties}
+          variants={createAnimateOnScroll(0.1)}>
+          {diamondPackage.map((partner, index) => (
+            <div key={index} className="diamond-container">
+              <img
+                src={`../../../public/logos/${partner.imageSource}.png`}
+                className="diamond-logo"></img>
+            </div>
+          ))}
+        </m.div>
+        <m.div
+          className="gold-section"
+          initial="hidden"
+          whileInView="visible"
+          viewport={cardViewportProperties}
+          variants={createAnimateOnScroll(0.1)}>
+          {goldPackage.map((partner, index) => (
+            <div key={index} className="gold-container">
+              <img
+                src={`../../../public/logos/${partner.imageSource}.png`}
+                className="gold-logo"></img>
+            </div>
+          ))}
+        </m.div>
+        <m.div
+          className="silver-section"
+          initial="hidden"
+          whileInView="visible"
+          viewport={cardViewportProperties}
+          variants={createAnimateOnScroll(0.1)}>
+          {silverPackage.map((partner, index) => (
+            <div key={index} className="silver-container">
+              <img
+                src={`../../../public/logos/${partner.imageSource}.png`}
+                className="silver-logo"></img>
+            </div>
+          ))}
+        </m.div>
+        <h2 id="patrons-header">{languageMode == 'polish' ? 'Patronat' : 'Patronage'}</h2>
+        <m.div
+          className="patrons-section"
+          initial="hidden"
+          whileInView="visible"
+          viewport={cardViewportProperties}
+          variants={createAnimateOnScroll(0.1)}>
+          <div className="patrons-container">
+            <img src="../../../public/logos/mlodzi_w_lodzi.png" className="patrons-logo"></img>
+          </div>
+        </m.div>
+      </div>
+    </>
   );
 };
 
