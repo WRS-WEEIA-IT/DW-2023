42ad7cf057f1d0a0310e357cbdda4573eedb62ea
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-01-25 17:05:12 +0100

    update npm package

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import EventsSection from '../Sections/EventsSection/EventsSection';
 import AboutSection from '../Sections/DescriptionSection/AboutSection';
 import PartnersSection from '../Sections/PartnersSection/PartnersSection';
 import Footer from '../Footer/Footer';
-import { domAnimation, LazyMotion } from 'framer-motion';
+import { LazyMotion, domAnimation } from 'framer-motion';
 
 const HomePage = () => {
   const setCustomViewportHeightVariable = () => {
