63899ecd7132c35dacb5fcbe7bdc4f6e0ae94121
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-01-25 16:59:31 +0100

    checkpoint before updating vite

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,11 +1,10 @@
 import Navbar from '../Navbar/Navbar';
 import './HomePage.scss';
-// import WelcomeSection from '../Sections/WelcomeSection/WelcomeSection';
-// import EventsSection from '../Sections/EventsSection/EventsSection';
-// import AboutSection from '../Sections/DescriptionSection/AboutSection';
-// import PartnersSection from '../Sections/PartnersSection/PartnersSection';
-// import Footer from '../Footer/Footer';
-import Button from '../../styles/Button.module.scss';
+import WelcomeSection from '../Sections/WelcomeSection/WelcomeSection';
+import EventsSection from '../Sections/EventsSection/EventsSection';
+import AboutSection from '../Sections/DescriptionSection/AboutSection';
+import PartnersSection from '../Sections/PartnersSection/PartnersSection';
+import Footer from '../Footer/Footer';
 import { domAnimation, LazyMotion } from 'framer-motion';
 
 const HomePage = () => {
@@ -17,21 +16,15 @@ const HomePage = () => {
 
   return (
     <LazyMotion features={domAnimation}>
-      {/* <div id="homepage-container"> */}
-      {/* <div id="backgroud-photo" /> */}
-      {/* <Navbar /> */}
-      <div>
-        <button className={`${Button.button} ${Button.filled} ${Button.square}`}>Zapisz się</button>
-        <button className={`${Button.button} ${Button.outlined} ${Button.round}`}>
-          Zapisz się
-        </button>
-      </div>
-      {/* <WelcomeSection />
+      <div id="homepage-container">
+        <div id="backgroud-photo" />
+        <Navbar />
+        <WelcomeSection />
         <AboutSection />
         <EventsSection />
         <PartnersSection />
-        <Footer /> */}
-      {/* </div> */}
+        <Footer />
+      </div>
     </LazyMotion>
   );
 };
