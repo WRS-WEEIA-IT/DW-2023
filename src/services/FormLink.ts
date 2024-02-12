7b438c8e307574bfd4db60cbca18a55b88cacd9c
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-02-12 20:29:01 +0100

    change form link

--- src/services/FormLink.ts
+++ src/services/FormLink.ts
@@ -1,2 +1,1 @@
-export const FORM_LINK =
-  'https://docs.google.com/forms/d/e/1FAIpQLSeFIvOGhhrMT6bvAaxosJENssDEa4kW-4ZJr0LpDQsLagVoNQ/closedform';
+export const FORM_LINK = 'https://forms.gle/XjRZccrbxortTEiU6';
