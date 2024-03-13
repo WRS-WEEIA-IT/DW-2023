3e53402d0ac73d643c18f534cd577026c37b1e01
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-03-13 18:27:51 +0100

    change form link

--- src/services/Links.ts
+++ src/services/Links.ts
@@ -1,4 +1,4 @@
-export const FORM_LINK = 'https://forms.gle/HYtHmMNQLsE89aBp7';
+export const FORM_LINK = 'https://forms.gle/MbbnSmdYNAZKH9tG8';
 export const APP_STORE_APP_LINK =
   'https://apps.apple.com/pl/app/dzie%C5%84-wydzia%C5%82u-eeia/id6446584873';
 export const GOOGLE_PLAY_APP_LINK = 'https://play.google.com/store/apps/details?id=com.app.dw2024';
