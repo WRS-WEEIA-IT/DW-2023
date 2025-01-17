f6aa4c50160a94d69f915b8f9f3742a520a67703
Olek Golus <olekgolus02@gmail.com>
2025-01-17 19:39:05 +0100

    Update site

--- src/services/Links.ts
+++ src/services/Links.ts
@@ -2,4 +2,4 @@ export const FORM_LINK = 'https://forms.gle/MbbnSmdYNAZKH9tG8';
 export const APP_STORE_APP_LINK =
   'https://apps.apple.com/pl/app/dzie%C5%84-wydzia%C5%82u-eeia/id6446584873';
 export const GOOGLE_PLAY_APP_LINK = 'https://play.google.com/store/apps/details?id=com.app.dw2024';
-export const FACEBOOK_EVENT_LINK = 'https://www.facebook.com/events/877959140777840/';
+export const FACEBOOK_EVENT_LINK = 'https://www.facebook.com/events/950646740375559/';
