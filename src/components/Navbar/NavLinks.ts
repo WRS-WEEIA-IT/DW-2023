c553e0c58358a4fb3434f2e064aabf0a47f2bc04
boshena <bozenab0411@gmail.com>
2026-02-06 18:52:52 +0100

    Zmiana kolejności w navbar

--- src/components/Navbar/NavLinks.ts
+++ src/components/Navbar/NavLinks.ts
@@ -11,15 +11,15 @@ const FOOTER_SECTION_ID = '#footer-section';
 
 export const navLinksPL: navLinkInterface[] = [
   { title: 'Strona Główna', sectionId: WELCOME_SECTION_ID },
-  { title: 'Harmonogram', sectionId: EVENTS_SECTION_ID },
   { title: 'Regulamin', sectionId: REGULAMIN_SECTION_ID },
+  { title: 'Harmonogram', sectionId: EVENTS_SECTION_ID },
   { title: 'Partnerzy', sectionId: PARTNERS_SECTION_ID },
   { title: 'Kontakt', sectionId: FOOTER_SECTION_ID },
 ];
 export const navLinksEN: navLinkInterface[] = [
   { title: 'Home', sectionId: WELCOME_SECTION_ID },
-  { title: 'Schedule', sectionId: EVENTS_SECTION_ID },
   { title: 'Terms', sectionId: REGULAMIN_SECTION_ID },
+  { title: 'Schedule', sectionId: EVENTS_SECTION_ID },
   { title: 'Partners', sectionId: PARTNERS_SECTION_ID },
   { title: 'Contact', sectionId: FOOTER_SECTION_ID },
 ];
