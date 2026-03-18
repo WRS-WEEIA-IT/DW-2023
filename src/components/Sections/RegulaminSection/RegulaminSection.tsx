11fec1dd69e0479832d18672fe1a4a251b7854db
Koxu1337 <koxu1337@gmail.com>
2026-03-18 07:35:11 +0100

    regulamin szkol

--- src/components/Sections/RegulaminSection/RegulaminSection.tsx
+++ src/components/Sections/RegulaminSection/RegulaminSection.tsx
@@ -15,10 +15,17 @@ const RegulaminSection = () => {
     languageMode === "polish"
       ? "Przed uczestnictwem w naszym wydarzeniu, prosimy o zapoznanie się z regulaminem."
       : "Before participating in our event, please read our terms and conditions.";
-  const BUTTON_TEXT = languageMode === "polish" ? "Przeczytaj regulamin" : "Read Terms";
 
-  const regulaminLink =
-    "https://zosayoqrngvrydnexaek.supabase.co/storage/v1/object/public/DW-files/regulamin.pdf";
+  const regulations = [
+    {
+      date: languageMode === "polish" ? "18 marca - Dzień Wydziału" : "March 18 - Department Day",
+      link: "https://zosayoqrngvrydnexaek.supabase.co/storage/v1/object/public/DW-files/regulamin.pdf",
+    },
+    {
+      date: languageMode === "polish" ? "19 marca - Dzień Szkół" : "March 19 - Schools Day",
+      link: "https://zosayoqrngvrydnexaek.supabase.co/storage/v1/object/public/DW-files/regulamin_szkoly.pdf",
+    },
+  ];
 
   return (
     <section id="regulamin-section">
@@ -30,14 +37,19 @@ const RegulaminSection = () => {
       >
         <h2 id="regulamin-header">{HEADER_TEXT}</h2>
         <p id="regulamin-description">{DESCRIPTION_TEXT}</p>
-        <a
-          href={regulaminLink}
-          className="regulamin-button"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {BUTTON_TEXT}
-        </a>
+        <div className="regulamin-buttons-container">
+          {regulations.map((regulation) => (
+            <a
+              key={regulation.date}
+              href={regulation.link}
+              className="regulamin-button"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {regulation.date}
+            </a>
+          ))}
+        </div>
       </m.div>
     </section>
   );
